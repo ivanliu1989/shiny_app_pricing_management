b81ef9f249e8a8d9775dc9da3f2c4ef713cd0622
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-20 16:25:07 +1000

    table

--- ui.R
+++ ui.R
@@ -1,28 +1,34 @@
 library(shiny)
 library(ggplot2)  # for the diamonds dataset
 
-shinyUI(fluidPage(
-    headerPanel("AfterPlus Pricing Management"),
-    sidebarLayout(
-        sidebarPanel(
-            conditionalPanel(
-                'input.dataset === "Dongdamen"',
-                checkboxGroupInput('show_vars_tb1','Columns in Dongdamen to show:',
-                                   names(diamonds), selected = names(diamonds))
-                ),
-            conditionalPanel(
-                'input.dataset === "GongCha"',
-                checkboxGroupInput('show_vars_tb2','Columns in Dongdamen to show:',
-                                   names(mtcars), selected = names(mtcars))
-                )
-            ),
-        mainPanel(
-            tabsetPanel(
-                id='dataset',
-                tabPanel('Dongdamen', dataTableOutput('mytable1')),
-                tabPanel('GongCha', dataTableOutput('mytable2'))
-                )
-            )
+shinyUI(navbarPage("AfterPlus Pricing Management", inverse = FALSE, collapsable = FALSE,
+                   tabPanel("Summary"),
+                   tabPanel("Detailed Report",
+                            sidebarLayout(
+                                sidebarPanel(
+                                    conditionalPanel(
+                                        'input.dataset === "Dongdamen"',
+                                        checkboxGroupInput('show_vars_tb1','Columns in Dongdamen to show:',
+                                                           names(diamonds), selected = names(diamonds))
+                                    ),
+                                    conditionalPanel(
+                                        'input.dataset === "GongCha"',
+                                        checkboxGroupInput('show_vars_tb2','Columns in Dongdamen to show:',
+                                                           names(mtcars), selected = names(mtcars))
+                                    )
+                                ),
+                                mainPanel(
+                                    tabsetPanel(
+                                        id='dataset',
+                                        tabPanel('Dongdamen', dataTableOutput('mytable1')),
+                                        tabPanel('GongCha', dataTableOutput('mytable2'))
+                                    )
+                                )
+                            )),
+                   navbarMenu("More",
+                              tabPanel("Upload Dataset"),
+                              tabPanel("Download Dataset"))
+                   
+                   
+                   )
         )
-    )
-)
