bf312673be724f2b41ba247bd7ccef6c5b4e245d
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-22 12:22:38 +1000

    dataset

---
+++ test.R
@@ -0,0 +1,17 @@
+setwd(choose.dir())
+require(xlsx)
+
+profits <- 0.3
+exchangeRate <- 5.8
+
+ap <- read.xlsx("CAT2.xlsx",1,encoding = "UTF-8",stringsAsFactors=FALSE)
+ap <- ap[,-8]
+if((ap$Currency == 'CNY'))
+    ap[,c(4,6)]<- ap[,c(4,6)]/exchangeRate
+ap$Cost <- ap$Unit.Cost + ap$Postage
+ap$Total.Cost <- ap$Cost * ap$Qty 
+ap$Sale.Price <- ap$Cost * (profits+1)
+ap$Unit.Margin <- ap$Sale.Price - ap$Cost
+
+
+head(ap)
